59f5fc794c67e151dabaf29f1a820b72336df039
ryshino <ranature1153@gmail.com>
2022-11-20 15:52:57 +0900

    デプロイに向けて設定を変更

--- config/puma.rb
+++ config/puma.rb
@@ -7,6 +7,12 @@
 max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
+port        ENV.fetch("PORT") { 3000 }
+environment ENV.fetch("RAILS_ENV") { "development" }
+pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
+workers ENV.fetch("WEB_CONCURRENCY") { 4 }
+preload_app!
+plugin :tmp_restart
 
 # Specifies the `worker_timeout` threshold that Puma will use to wait before
 # terminating a worker in development environments.
