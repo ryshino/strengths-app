349e0d10f869eff7004c748de181d6effc27a112
ryshino <ranature1153@gmail.com>
2022-11-21 10:00:15 +0900

    ログインフォームを追加

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(profile: params[:session][:profile])
+    if user && user.authenticate(params[:session][:password])
+      reset_session
+      log_in user
+      redirect_to user
+    else
+      flash.now[:danger] = 'パスワードかプロフィールURLが正しくありません'
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+  end
+end
