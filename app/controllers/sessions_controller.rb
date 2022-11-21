67b986bc9303901494a109797c5f053088baa50a
ryshino <ranature1153@gmail.com>
2022-11-21 14:29:48 +0900

    ログアウト機能の実装とログインテストのリファクタリング

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,7 +13,9 @@ class SessionsController < ApplicationController
       render 'new', status: :unprocessable_entity
     end
   end
-
+  
   def destroy
+    log_out
+    redirect_to root_url, status: :see_other
   end
 end
