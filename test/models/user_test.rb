014d1305b2b79bc164fa9abaa521e4be5f64720f
ryshino <ranature1153@gmail.com>
2022-11-23 18:18:35 +0900

    ログイン情報を永続化

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -63,4 +63,8 @@ class UserTest < ActiveSupport::TestCase
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end
+
+  test "ダイジェストが存在しない場合のauthenticated?のテスト" do
+    assert_not @user.authenticated?('')
+  end
 end
