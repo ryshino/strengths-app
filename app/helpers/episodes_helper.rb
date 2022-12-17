7cec88170b4f8be039c2b1e324a8baca461325a0
ryshino <ranature1153@gmail.com>
2022-12-17 16:58:11 +0900

    改行を反映させるメソッドを定義

--- app/helpers/episodes_helper.rb
+++ app/helpers/episodes_helper.rb
@@ -1,2 +1,7 @@
 module EpisodesHelper
+  # 改行を反映させるためメソッド
+  # http://taustation.com/rails-reflecting-newline-code/
+  def html_safe_newline(str)
+    h(str).gsub(/\n|\r|\r\n/, "<br>").html_safe
+  end
 end
