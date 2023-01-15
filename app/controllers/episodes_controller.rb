a325094f690a763efdc911acae1a9a608aaa2e5b
ryshino <ranature1153@gmail.com>
2023-01-15 15:53:15 +0900

    コードレビューしてもらった点を修正 select から having に変更

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -14,9 +14,8 @@ class EpisodesController < ApplicationController
   def show
     @episode = Episode.find(params[:id])
     @tag_relation = TagRelation.new
-    # 1つにまとめるのと処理を2つに分けるのはどっちが良いんだろう？
-    select_tags = Episode.joins(tag_relations: :tag).group(:episode_id, :name).size.
-                  select { |k| k.slice(0) == @episode.id }
+    select_tags = Episode.joins(tag_relations: :tag).group(:episode_id, :name).
+                  having("episode_id == ?", @episode.id).size
     @episode_tags = select_tags.map { |k, v| [k.slice(1), v] }
   end
 
