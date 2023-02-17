233d044ce7883e380cdb3232cb569686d074889b
ryshino <ranature1153@gmail.com>
2023-02-17 11:45:45 +0900

    kaminari を導入

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -4,7 +4,7 @@ class EpisodesController < ApplicationController
   before_action :set_q, only: [:index]
 
   def index
-    @episodes = @q.result.paginate(page: params[:page])
+    @episodes = @q.result.page(params[:page]).per(10)
     @episode  = current_user.episodes.build
     if @episodes.blank?
       flash.now[:success] = "該当のエピソードは見つかりませんでした。"
