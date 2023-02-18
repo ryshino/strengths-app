2c2ea51f99519b12200a83b08e394064e8f1baff
ryshino <ranature1153@gmail.com>
2023-02-18 21:10:25 +0900

    エピソード一覧にタグを表示

--- app/helpers/episodes_helper.rb
+++ app/helpers/episodes_helper.rb
@@ -4,4 +4,16 @@ module EpisodesHelper
   def html_safe_newline(str)
     h(str).gsub(/\n|\r|\r\n/, "<br>").html_safe
   end
+
+  def episode_tag_name(episode)
+    episode.tags.group(:name).count.first.first
+  end
+
+  def episode_tag_count(episode)
+    episode.tags.group(:name).count.first.second
+  end
+
+  def other_episode_tas_count(episode)
+    episode.tags.group(:name).drop(1).count
+  end
 end
