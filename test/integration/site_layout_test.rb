eb077e896fa4022a6424430213c4b03bb94de03f
ryshino <ranature1153@gmail.com>
2023-03-18 12:25:37 +0900

    ホームページのテストを修正

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -9,13 +9,12 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   test "リンクが正しく表示されているかテスト" do
     get root_path
     assert_template 'static_pages/home'
-    assert_select "a[href=?]", root_path, count: 2
-    assert_select "a[href=?]", about_path
-    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", root_path
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", signup_path
-    get contact_path
-    assert_select "title", full_title("Contact")
+    assert_select "a[href=?]", "https://qiita.com/yatchi_323/private/968f9121139472fceffe", "使い方を見る"
+    get login_path
+    assert_select "title", full_title("ログイン")
     get signup_path
     assert_select "title", full_title("新規登録")
   end
@@ -24,14 +23,13 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     log_in_as(@user)
     get root_path
     assert_template 'static_pages/home'
-    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", root_path
     assert_select "a[href=?]", users_path
-    assert_select "a[href=?]", about_path
-    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", episodes_path
+    assert_select "a[href=?]", new_episode_path
     assert_select "a[href=?]", user_path(@user)
     assert_select "a[href=?]", edit_user_path(@user)
     assert_select "a[href=?]", logout_path
-    assert_match @user.followers.count.to_s, response.body
-    assert_match @user.following.count.to_s, response.body
+    assert_select "a[href=?]", "https://qiita.com/yatchi_323/private/968f9121139472fceffe", "使い方を見る"
   end
 end
