d7ee1f10589fb0b27edcb7551551c542b81add3f
ryshino <ranature1153@gmail.com>
2023-03-18 09:53:51 +0900

    改行コードをリファクタリング

--- app/helpers/episodes_helper.rb
+++ app/helpers/episodes_helper.rb
@@ -2,7 +2,7 @@ module EpisodesHelper
   # 改行を反映させるためメソッド
   # http://taustation.com/rails-reflecting-newline-code/
   def html_safe_newline(str)
-    h(str).gsub(/\n|\r|\r\n/, "<br>").html_safe
+    safe_join(str.split(/[\r\n]+/), tag.br).html_safe
   end
 
   def episode_tags_info(episode)
