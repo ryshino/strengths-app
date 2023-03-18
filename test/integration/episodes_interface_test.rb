f9b27d1b6218d1996c59c331b0101829ec480e28
ryshino <ranature1153@gmail.com>
2023-03-18 16:34:58 +0900

    エピソード関係のテストを修正

--- test/integration/episodes_interface_test.rb
+++ test/integration/episodes_interface_test.rb
@@ -10,17 +10,11 @@ end
 
 class EpisodesInterfaceTest < EpisodesInterface
 
-  test "ページネーションがあるかテスト" do
-    get root_path
-    assert_select 'div.pagination'
-  end
-
   test "エラーを表示し、無効な送信に対してエピソードを作成しているかテスト" do
     assert_no_difference 'Episode.count' do
       post episodes_path, params: { episode: { content: "", tag_ids: [@tag.id] } }
     end
     assert_select 'div#error_explanation'
-    assert_select 'a[href=?]', '/?page=2'  # 正しいページネーションリンク
   end
 
   test "エピソードが作成されているかテスト" do
@@ -44,7 +38,7 @@ class EpisodesInterfaceTest < EpisodesInterface
   end
 
   test "自分の投稿を削除できるかテストe" do
-    first_episode = @user.episodes.paginate(page: 1).first
+    first_episode = @user.episodes.page(1).first
     assert_difference 'Episode.count', -1 do
       delete episode_path(first_episode)
     end
@@ -53,25 +47,5 @@ class EpisodesInterfaceTest < EpisodesInterface
   test "他のユーザーの投稿に対しては削除リンクが存在しないことをテスト" do
     get user_path(users(:archer))
     assert_select 'a', { text: '削除', count: 0 }
-  end
-end
-
-class EpisodeSidebarTest < EpisodesInterface
-
-  test "正しいエピソード数が表示されているかテスト" do
-    get root_path
-    assert_match "#{ @user.episodes.count } episodes", response.body
-  end
-
-  test "投稿数が0の時適切な形で表示しているかテスト" do
-    log_in_as(users(:malory))
-    get root_path
-    assert_match "0 episodes", response.body
-  end
-
-  test "投稿数が1の時適切な形で表示しているかテスト" do
-    log_in_as(users(:lana))
-    get root_path
-    assert_match "1 episode", response.body
   end
 end
