6bc07410647774028552a16f07e3c6285eb716b8
ryshino <ranature1153@gmail.com>
2023-04-08 15:54:09 +0900

    seedsファイルを実行できるように修正

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,5 +5,5 @@ set -o errexit
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-bundle exec rake db:migrate:reset
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 rails db:migrate:reset
 bundle exec rails db:seed
